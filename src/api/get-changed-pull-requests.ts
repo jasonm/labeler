7834b71d7295ec50842ab259f499bd38d1db4c83
Jason Morrison <jason.p.morrison@gmail.com>
2025-05-14 09:24:21 -0700

    Update get-changed-pull-requests.ts

--- src/api/get-changed-pull-requests.ts
+++ src/api/get-changed-pull-requests.ts
@@ -19,7 +19,7 @@ export async function* getPullRequests(
       prData = result.data;
     } catch (error: any) {
       core.warning(error);
-      core.warning(`Could not find pull request #${prNumber}, skipping`);
+      core.warning(`!!! Could not find pull request #${prNumber}, skipping`);
       continue;
     }
 
